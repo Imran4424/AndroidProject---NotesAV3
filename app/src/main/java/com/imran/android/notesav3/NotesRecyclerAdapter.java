d79c18d8438c5a07a469b097ea51af66c218786e
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-18 02:27:37 +0600

    item note list

--- app/src/main/java/com/imran/android/notesav3/NotesRecyclerAdapter.java
+++ app/src/main/java/com/imran/android/notesav3/NotesRecyclerAdapter.java
@@ -1,8 +1,51 @@
 package com.imran.android.notesav3;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
 /**
  * Created by Shah Md Imran Hossain on 18, November, 2020
  */
-public class NotesRecyclerAdapter {
+public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder> {
+    private final Context context;
+    private final LayoutInflater layoutInflater;
+    private final List<String> titles;
+    private final List<String> body;
 
+    public NotesRecyclerAdapter(Context context, List<String> titles, List<String> body) {
+        this.context = context;
+        layoutInflater = LayoutInflater.from(context);
+        this.titles = titles;
+        this.body = body;
+    }
+
+    @NonNull
+    @Override
+    public NotesRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotesRecyclerAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
