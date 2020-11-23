fbfea3dfe57035a146407817cdffe2a1d2658418
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-24 00:09:25 +0600

    editor activity

--- app/src/main/java/com/imran/android/notesav3/NoteEditorActivity.java
+++ app/src/main/java/com/imran/android/notesav3/NoteEditorActivity.java
@@ -3,6 +3,8 @@ package com.imran.android.notesav3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.TextView;
 
 public class NoteEditorActivity extends AppCompatActivity {
@@ -16,5 +18,39 @@ public class NoteEditorActivity extends AppCompatActivity {
 
         title = findViewById(R.id.editorTitle);
         body = findViewById(R.id.editTextTextMultiLine);
+
+        title.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        body.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
